d4895c8d297f76ebc49690757d2e9a0a391ac561
TomekRogal <tomekrogalski@vp.pl>
2023-05-12 12:01:37 +0200

    Dodawanie i usuwanie

--- TaskManager/src/main/java/pl/coderslab/TaskManager.java
+++ TaskManager/src/main/java/pl/coderslab/TaskManager.java
@@ -1,19 +1,26 @@
 package pl.coderslab;
 
 
+import org.apache.commons.lang3.ArrayUtils;
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TaskManager {
     static String tasks[][];
+    static String fileTask = "tasks.csv";
     public static void main(String[] args) {
-        showOptions();
+
         tasks = tasksFromFile("tasks.csv");
-        System.out.println(tasks[0][2]);
+        showOptions();
+        optionSelect();
     }
 
     public static void showOptions() {
@@ -56,8 +63,93 @@ public class TaskManager {
         }
         return taskArray;
     }
-    public static void optionSelect(){
+    public static void optionSelect() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose option:");
+        String input = "";
+        while (!"exit".equals(input)) {
+            input = scanner.nextLine();
+            switch (input) {
+                case "add":
+                    addTask();
+                    showOptions();
+                    break;
+                case "remove":
+                    removeTask();
+                    showOptions();
+                    break;
+                case "list":
+                    listTask();
+                    showOptions();
+                    break;
+                case "exit":
+                    exitAndSave();
+                    System.out.println(ConsoleColors.RED + "Bye Bye");
+                    break;
+                default:
+                    System.out.println("Please select a correct option!!");
+            }
+        }
+    }
+    public static void addTask (){
+        tasks = Arrays.copyOf(tasks,tasks.length+1);
+        tasks[tasks.length-1] = new String[3];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please add task description");
+        tasks[tasks.length-1][0]=scanner.nextLine();
+        System.out.println("Please add task due date");
+        tasks[tasks.length-1][1]=scanner.nextLine();
+        System.out.println("Is your task is important: true/false");
+        tasks[tasks.length-1][2]=scanner.nextLine();
+    }
+    public static void listTask(){
+       int counter = 0;
+        for (int i = 0; i < tasks.length; i++) {
+            System.out.print(i + " : ");
+            for (int j = 0; j < tasks[i].length; j++) {
+                System.out.print(tasks[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void removeTask(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please select number to remove");
+        int counter = 0;
+        while(counter == 0) {
+            try {
+
+                int index = getInt();
+                tasks = ArrayUtils.remove(tasks, index);
+                System.out.println("Value was successflly deleted");
+                counter +=1;
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("Incorect argument passed. Please give correct index number");
+            }
+        }
 
     }
+    public static void exitAndSave(){
+        try (FileWriter fileWriter = new FileWriter(fileTask,false)) {
+            for (int i = 0; i < tasks.length; i++) {
+                for (int j = 0; j < tasks[i].length; j++) {
+                    fileWriter.append(tasks[i][j]);
+                }
+                fileWriter.append("\n");
+            }
+        } catch (IOException e) {
+            System.out.println("File Save Error");
+        }
+
+    }
+    public static int getInt() {
+        Scanner scanner = new Scanner(System.in);
+        while(!scanner.hasNextInt()) {
+            System.out.println("Incorect argument passed. Please give correct number");
+            scanner.next();
+        }
+        return scanner.nextInt();
+    }
+
 }
 
